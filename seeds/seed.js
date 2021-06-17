51836f50c5a70b12c3386da66d770ee7ffea8aac
Melvin Finn <melvinrf@Outlook.com>
2021-06-17 09:27:51 -0500

    added seed script for databse

---
+++ seeds/seed.js
@@ -0,0 +1,17 @@
+const sequelize = require('../config/connection');
+const { Post } = require('../models');
+
+const postData = require('./postData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    await Post.bulkCreate(postData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+seedDatabase();
