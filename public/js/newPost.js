b907d312857001784763c4d97ec94442ff89762c
melvin <melvinrf@outlook.com>
2021-09-27 14:20:03 -0700

    updated header

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -1,24 +1,23 @@
 const posthandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const title = document.querySelector('#title-input').value.trim();
-    const body = document.querySelector('#body-input').value.trim();
+  const title = document.querySelector("input[type='text']").value.trim();
+  const body = document.querySelector("input[type='submit']").value.trim();
 
-    if (title && body) {
-        const response = await fetch('/api/post', {
-            method: 'POST',
-            body: JSON.strinfigify({ title, body }),
-            headers: { 'Content-Type': 'application/json' },
-        });
+  if (title && body) {
+    const response = await fetch("/api/post", {
+      method: "POST",
+      body: JSON.strinfigify({ title, body }),
+      headers: { "Content-Type": "application/json" },
+    });
+    console.log(title, body);
 
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            alert('Failed to log in.');
-        }
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to log in.");
     }
+  }
 };
 
-document
-    .querySelector('newpost-form')
-    .addEventListener('submit', posthandler);
+document.querySelector("form").addEventListener("submit", posthandler);
